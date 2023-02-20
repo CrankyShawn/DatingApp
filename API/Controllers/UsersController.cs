6208fc90b560cf5d7d99a048e831cf936762bf84
CrankyShawn <svinje@crankyape.com>
2023-02-20 15:14:36 -0600

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
 
             return users;
         }
-
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
